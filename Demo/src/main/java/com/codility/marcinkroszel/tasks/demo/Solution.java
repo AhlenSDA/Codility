1ad6962b14da17848e6fe01b77eaba036a2d6ca0
AhlenSDA <ahlensda@wp.pl>
2017-12-01 23:11:04 +0100

    Missing if statement added

--- Demo/src/main/java/com/codility/marcinkroszel/tasks/demo/Solution.java
+++ Demo/src/main/java/com/codility/marcinkroszel/tasks/demo/Solution.java
@@ -7,11 +7,11 @@ public class Solution {
     // int[] arrays created for testing different algorithm variation
     private static int[] exampleArray1 = {-1};
     private static int[] exampleArray2 = {1};
-    private static int[] exampleArray3 = {0, 1, 1, 2, 3, 5, -1, 0, -1, 0, -1, 5, 7, -100, -2, 3, 4, 0, -1};
+    private static int[] exampleArray3 = {0};
     private static int[] exampleArray4 = {0, 1, 2, 3, 4};
     private static int[] exampleArray5 = {-1, -3};
     private static int[] exampleArray6 = {1, 1, 1};
-    private static int[] exampleArray7 = {1, 1};
+    private static int[] exampleArray7 = {-1, -1};
     private static int[] exampleArray8 = {1, 1, 1, 2, 2, 2};
     private static int[] exampleArray9 = {0, 0, 0,};
 
@@ -33,11 +33,13 @@ public class Solution {
             }
         }
 
-        for (int i = 0; i < A.length - 1; i++) {
-            if ((A[i] >= 0) && ((A[i + 1] - A[i]) > 1)) {
-                System.out.println("Searched N = " + (result = A[i] + 1) + "\n");
-                resultFlag = true;
-                break;
+        if (!resultFlag) {
+            for (int i = 0; i < A.length - 1; i++) {
+                if ((A[i] >= 0) && ((A[i + 1] - A[i]) > 1)) {
+                    System.out.println("Searched N = " + (result = A[i] + 1) + "\n");
+                    resultFlag = true;
+                    break;
+                }
             }
         }
 
