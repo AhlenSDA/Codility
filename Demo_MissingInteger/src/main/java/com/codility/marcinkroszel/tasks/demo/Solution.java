b371b72e92ecdd8de46b19dae974708ab49dba23
AhlenSDA <ahlensda@wp.pl>
2017-12-01 23:45:06 +0100

    Missing folder added.

---
+++ Demo_MissingInteger/src/main/java/com/codility/marcinkroszel/tasks/demo/Solution.java
@@ -0,0 +1,89 @@
+package com.codility.marcinkroszel.tasks.demo;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    // int[] arrays created for testing different algorithm variation
+    private static int[] exampleArray1 = {0};
+    private static int[] exampleArray2 = {1};
+    private static int[] exampleArray3 = {-1};
+    private static int[] exampleArray4 = {50000};
+    private static int[] exampleArray5 = {-100000, 100000, 0};
+    private static int[] exampleArray6 = {10000000, -1000000, 0};
+    private static int[] exampleArray7 = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    private static int[] exampleArray8 = {1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 1000, 1000, 1000, 0 , -100000};
+    private static int[] exampleArray9 = {0, 0, 0,};
+
+    public int solution(int[] A) {
+
+        System.out.println("Unordered array: " + Arrays.toString(A));
+        Arrays.sort(A);
+        System.out.println("Ascending array: " + Arrays.toString(A));
+
+        // boolean allows separation of each checked condition
+        boolean resultFlag = false; // if value is set to false, next condition is entered and int[] is checked
+        int result = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if ((A[i] >= 0) && ((A.length == 1))) {
+                System.out.println("Searched N = " + (result = A[i] + 1) + "\n");
+                resultFlag = true; // if value is set to true, no more following condition are checked
+                break;
+            }
+        }
+
+        if (!resultFlag) {
+            for (int i = 0; i < A.length - 1; i++) {
+                if ((A[i] >= 0) && ((A[i + 1] - A[i]) > 1)) {
+                    System.out.println("Searched N = " + (result = A[i] + 1) + "\n");
+                    resultFlag = true;
+                    break;
+                }
+            }
+        }
+
+        if (!resultFlag) {
+            for (int i = 0; i < A.length; i++) {
+                if ((A[i] < 0)) {
+                    System.out.println("Searched N = " + (result = 1) + "\n");
+                    resultFlag = true;
+                    break;
+                }
+            }
+        }
+
+        if (!resultFlag) {
+            for (int i = 0; i < A.length; i++) {
+                if ((A[i + 1] == A[i]) || (A[i + 1] - A[i]) == 1) {
+                    System.out.println("Searched N = " + (result = A[A.length - 1] + 1) + "\n");
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Solution demo = new Solution();
+        System.out.println("Array_1");
+        demo.solution(exampleArray1);
+        System.out.println("Array_2");
+        demo.solution(exampleArray2);
+        System.out.println("Array_3");
+        demo.solution(exampleArray3);
+        System.out.println("Array_4");
+        demo.solution(exampleArray4);
+        System.out.println("Array_5");
+        demo.solution(exampleArray5);
+        System.out.println("Array_6");
+        demo.solution(exampleArray6);
+        System.out.println("Array_7");
+        demo.solution(exampleArray7);
+        System.out.println("Array_8");
+        demo.solution(exampleArray8);
+        System.out.println("Array_9");
+        demo.solution(exampleArray9);
+    }
+}
